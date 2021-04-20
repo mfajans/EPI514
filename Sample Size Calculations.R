53d596df301e7e46752e0ec2469137fa7816681d
mfajans <81601353+mfajans@users.noreply.github.com>
2021-04-20 14:02:25 -0700

    Add files via upload
    Initial sample size calculations for the brief research proposal

---
+++ Sample Size Calculations.R
@@ -0,0 +1,301 @@
+#########################
+#EPI514 Project;
+#Sample Size Calculations
+#########################
+
+library(epiR)
+
+#Calculating minimum detectable prevalence ratio of influenza vaccine coverage
+#between participants identifying as transgender and general population
+
+#Assumptions: 0.05 alpha
+#             0.8 power
+#             Sample Size= 1,037,079
+
+
+#800 sample size, 40% unexposed
+
+epi.ssxsectn(
+pdexp1=NA,
+pdexp0=0.4,
+n=1037079,
+power=0.8,
+r=800/(1037079-800)
+)
+
+#800 sample size, 45% unexposed
+
+epi.ssxsectn(
+        pdexp1=NA,
+        pdexp0=0.45,
+        n=1037079,
+        power=0.8,
+        r=800/(1037079-800)
+)
+
+#800 sample size, 50% unexposed
+
+epi.ssxsectn(
+        pdexp1=NA,
+        pdexp0=0.5,
+        n=1037079,
+        power=0.8,
+        r=800/(1037079-800)
+)
+
+#800 sample size, 55% unexposed
+
+epi.ssxsectn(
+        pdexp1=NA,
+        pdexp0=0.55,
+        n=1037079,
+        power=0.8,
+        r=800/(1037079-800)
+)
+
+
+#800 sample size, 60% unexposed
+
+epi.ssxsectn(
+        pdexp1=NA,
+        pdexp0=0.6,
+        n=1037079,
+        power=0.8,
+        r=800/(1037079-800)
+)
+
+
+
+
+
+#1000 sample size, 40% unexposed
+
+epi.ssxsectn(
+        pdexp1=NA,
+        pdexp0=0.4,
+        n=1037079,
+        power=0.8,
+        r=1000/(1037079-1000)
+)
+
+#1000 sample size, 45% unexposed
+
+epi.ssxsectn(
+        pdexp1=NA,
+        pdexp0=0.45,
+        n=1037079,
+        power=0.8,
+        r=1000/(1037079-1000)
+)
+
+#1000 sample size, 50% unexposed
+
+epi.ssxsectn(
+        pdexp1=NA,
+        pdexp0=0.5,
+        n=1037079,
+        power=0.8,
+        r=1000/(1037079-1000)
+)
+
+#1000 sample size, 55% unexposed
+
+epi.ssxsectn(
+        pdexp1=NA,
+        pdexp0=0.55,
+        n=1037079,
+        power=0.8,
+        r=1000/(1037079-1000)
+)
+
+
+#1000 sample size, 60% unexposed
+
+epi.ssxsectn(
+        pdexp1=NA,
+        pdexp0=0.6,
+        n=1037079,
+        power=0.8,
+        r=1000/(1037079-1000)
+)
+
+
+
+
+
+#2000 sample size, 40% unexposed
+
+epi.ssxsectn(
+        pdexp1=NA,
+        pdexp0=0.4,
+        n=1037079,
+        power=0.8,
+        r=2000/(1037079-2000)
+)
+
+#2000 sample size, 45% unexposed
+
+epi.ssxsectn(
+        pdexp1=NA,
+        pdexp0=0.45,
+        n=1037079,
+        power=0.8,
+        r=2000/(1037079-2000)
+)
+
+#2000 sample size, 50% unexposed
+
+epi.ssxsectn(
+        pdexp1=NA,
+        pdexp0=0.5,
+        n=1037079,
+        power=0.8,
+        r=2000/(1037079-2000)
+)
+
+#2000 sample size, 55% unexposed
+
+epi.ssxsectn(
+        pdexp1=NA,
+        pdexp0=0.55,
+        n=1037079,
+        power=0.8,
+        r=2000/(1037079-2000)
+)
+
+
+#2000 sample size, 60% unexposed
+
+epi.ssxsectn(
+        pdexp1=NA,
+        pdexp0=0.6,
+        n=1037079,
+        power=0.8,
+        r=2000/(1037079-2000)
+)
+
+
+
+
+
+
+
+
+
+#2500 sample size, 40% unexposed
+
+epi.ssxsectn(
+        pdexp1=NA,
+        pdexp0=0.4,
+        n=1037079,
+        power=0.8,
+        r=2500/(1037079-2500)
+)
+
+#2500 sample size, 45% unexposed
+
+epi.ssxsectn(
+        pdexp1=NA,
+        pdexp0=0.45,
+        n=1037079,
+        power=0.8,
+        r=2500/(1037079-2500)
+)
+
+#2500 sample size, 50% unexposed
+
+epi.ssxsectn(
+        pdexp1=NA,
+        pdexp0=0.5,
+        n=1037079,
+        power=0.8,
+        r=2500/(1037079-2500)
+)
+
+#2500 sample size, 55% unexposed
+
+epi.ssxsectn(
+        pdexp1=NA,
+        pdexp0=0.55,
+        n=1037079,
+        power=0.8,
+        r=2500/(1037079-2500)
+)
+
+
+#2500 sample size, 60% unexposed
+
+epi.ssxsectn(
+        pdexp1=NA,
+        pdexp0=0.6,
+        n=1037079,
+        power=0.8,
+        r=2500/(1037079-2500)
+)
+
+
+
+
+
+#5000 sample size, 40% unexposed
+
+epi.ssxsectn(
+        pdexp1=NA,
+        pdexp0=0.4,
+        n=1037079,
+        power=0.8,
+        r=5000/(1037079-5000)
+)
+
+#5000 sample size, 45% unexposed
+
+epi.ssxsectn(
+        pdexp1=NA,
+        pdexp0=0.45,
+        n=1037079,
+        power=0.8,
+        r=5000/(1037079-5000)
+)
+
+#5000 sample size, 50% unexposed
+
+epi.ssxsectn(
+        pdexp1=NA,
+        pdexp0=0.5,
+        n=1037079,
+        power=0.8,
+        r=5000/(1037079-5000)
+)
+
+#5000 sample size, 55% unexposed
+
+epi.ssxsectn(
+        pdexp1=NA,
+        pdexp0=0.55,
+        n=1037079,
+        power=0.8,
+        r=5000/(1037079-5000)
+)
+
+
+#5000 sample size, 60% unexposed
+
+epi.ssxsectn(
+        pdexp1=NA,
+        pdexp0=0.6,
+        n=1037079,
+        power=0.8,
+        r=5000/(1037079-5000)
+)
+
+
+
+
+
+
+
+
+
+
+
+
